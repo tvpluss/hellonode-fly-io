b9582668ed6e1acda9a504335e66dd806f5ac8a0
Mateus <phuoctruong.vn@tribox.me>
2023-07-31 11:40:20 +0700

    update

--- server.js
+++ server.js
@@ -2,18 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get(["/", "/:name"], (req, res) => {
-  greeting = "<h1>Hello From Node on Fly!</h1>";
-  name = req.params["name"];
-  if (name) {
-    res.send(greeting + "</br>and hello to " + name);
-  } else {
-    res.send(greeting);
-  }
-});
-
 app.get("ping", (req, res) => {
   res.send("pong");
+});
+app.get(["/"], (req, res) => {
+  greeting = "<h1>Hello From Node on Fly!</h1>";
+  res.send(greeting);
 });
 
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
